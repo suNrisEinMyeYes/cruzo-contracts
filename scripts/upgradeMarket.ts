9b8a52384b2510d9195acd7963f10224f81d1df4
Roman Bobrik <bobrom27@gmail.com>
2022-07-15 22:22:01 +0300

    CC-55 - Deploy, Upgrade scripts refactoring. Minor ReadMe updates.

--- scripts/upgradeMarket.ts
+++ scripts/upgradeMarket.ts
@@ -1,32 +1,31 @@
-import { ethers, upgrades } from "hardhat";
-import * as fs from "fs";
-import hre from "hardhat";
+import { ethers, network, upgrades } from "hardhat";
+import { getAddress } from "../utils/addressTracking";
 
 async function main() {
-    const chainId = hre.network.config.chainId;
+  const chainId = network.config.chainId;
+  if (!chainId) {
+    throw "Chain ID is undefined, terminating";
+  }
+  const addressEntry = getAddress(chainId);
+  if (!addressEntry) {
+    throw "Address entry not found undefined, not possible to upgrade, terminating";
+  }
 
-    console.log("Upgrading market contract");
-    const Market = await ethers.getContractFactory("CruzoMarket");
+  console.log("Upgrading market contract");
+  const Market = await ethers.getContractFactory("CruzoMarket");
+  const market = await upgrades.upgradeProxy(addressEntry.market, Market);
+  await market.deployed();
 
-    let text = fs.readFileSync("networks.json", "utf8");
-    let obj = JSON.parse(text);
-    let data = { token: "", market: "" };
-    let contractsAddressMapping = new Map();
-    contractsAddressMapping = new Map(Object.entries(obj));
-    data = contractsAddressMapping.get(chainId?.toString());
-
-    let newMarket = await upgrades.upgradeProxy(data.market, Market);
-    await newMarket.deployed();
-
-    console.log("Market Contract upgraded");
-    console.log("New Market Contract Address : ", newMarket.address);
-    console.log(`https://polygonscan.com/token/${newMarket.address}`);
-    console.log(`https://mumbai.polygonscan.com/token/${newMarket.address}`);
+  console.log("Market Contract upgraded");
+  console.log("Market Contract Address : ", market.address);
+  // TODO: replace with appropriate website depending on the network
+  // console.log(`https://polygonscan.com/token/${market.address}`);
+  // console.log(`https://mumbai.polygonscan.com/token/${market.address}`);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
