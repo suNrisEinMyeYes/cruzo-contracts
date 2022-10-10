ec9b47997031ff50d648e86d9ede06e9e3c07ebf
Nikita Korshunov <kewka96@gmail.com>
2022-10-10 17:57:14 +0400

    fix(scripts): setVaultAddress in deployVault

--- scripts/deploy/deployVault.ts
+++ scripts/deploy/deployVault.ts
@@ -1,37 +1,45 @@
 import { ethers, network, upgrades } from "hardhat";
-import { ContractType, setAddress, getAddress } from "../../utils/addressTracking";
+import {
+  ContractType,
+  setAddress,
+  getAddress,
+} from "../../utils/addressTracking";
 
 async function main() {
-    const chainId = network.config.chainId;
-    if (!chainId) {
-        throw "Chain ID is undefined, terminating";
-    }
+  const chainId = network.config.chainId;
+  if (!chainId) {
+    throw "Chain ID is undefined, terminating";
+  }
 
-    console.log("Deploying vault contract");
-    const Vault = await ethers.getContractFactory("Cruzo1155Vault");
+  console.log("Deploying vault contract");
+  const Vault = await ethers.getContractFactory("Cruzo1155Vault");
+  const CruzoMarket = await ethers.getContractFactory("CruzoMarket");
 
-    let marketAddress = getAddress(chainId)!.market;
-    if (!marketAddress) {
-        throw `Market (${marketAddress}) address is undefined, terminating`;
-    }
+  let marketAddress = getAddress(chainId)!.market;
+  if (!marketAddress) {
+    throw `Market (${marketAddress}) address is undefined, terminating`;
+  }
 
-    const vault = await upgrades.deployProxy(Vault, [marketAddress], {
-        kind: "uups",
-    });
-    await vault.deployed();
+  const vault = await upgrades.deployProxy(Vault, [marketAddress], {
+    kind: "uups",
+  });
+  await vault.deployed();
+  await CruzoMarket.attach(marketAddress)
+    .setVaultAddress(vault.address)
+    .then((tx) => tx.wait());
 
-    console.log("Vault Contract Deployed");
-    console.log("Vault Contract Address : ", vault.address);
-    // TODO: replace with appropriate website depending on the network
-    // console.log(`https://polygonscan.com/token/${market.address}`);
-    // console.log(`https://mumbai.polygonscan.com/token/${market.address}`);
+  console.log("Vault Contract Deployed");
+  console.log("Vault Contract Address : ", vault.address);
+  // TODO: replace with appropriate website depending on the network
+  // console.log(`https://polygonscan.com/token/${market.address}`);
+  // console.log(`https://mumbai.polygonscan.com/token/${market.address}`);
 
-    setAddress(chainId, ContractType.vault, vault.address);
+  setAddress(chainId, ContractType.vault, vault.address);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
